84a5e570548a94933fce96b5499783a48872d5f3
Santiago Bello <isbelloq@gmail.com>
2022-12-06 21:27:59 -0500

    Dia 3 - parte 2 Modificaciones 	day_03/day3.py: Se agrega la solucion a la segunda parte del problema 	day_03/day_3_rucksack_reorganization.md: Se agrega el enunciado de la segunda parte del problema

--- day_03/day3.py
+++ day_03/day3.py
@@ -1,9 +1,12 @@
+from functools import reduce
 import os
 
 os.chdir(os.path.join(os.environ['HOME'], '2022_adventofcode/day_03'))
 
 points = {chr(x+97): x+1 for x in range(26)}
 points.update({chr(x+65): x+27 for x in range(26)})
+
+#Part 1
 
 with open('input', 'r') as f:
     total_points = 0
@@ -14,3 +17,14 @@ with open('input', 'r') as f:
         total_points += sum(points[x] for x in mixed_elements)
 
 print(f'Suma total de puntos: {total_points}')
+
+#Part 2
+with open('input', 'r') as f:
+    rucksack =[line.strip() for line in f]
+
+total_points = 0
+for i in range(0, len(rucksack), 3):
+    elements = reduce(lambda x,y: set(x).intersection(set(y)), rucksack[i:i+3])
+    total_points += sum(points[x] for x in elements)
+
+print(f'Suma total por equipos de puntos: {total_points}')

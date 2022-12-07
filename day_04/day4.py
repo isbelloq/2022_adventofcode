62d1826096e39c862eced164de5b93e08e84dd29
Santiago Bello <isbelloq@gmail.com>
2022-12-06 22:31:25 -0500

    Dia 4 - parte 2
    Modificaciones
    	day_04/day4.py: Se agrega la solucion de la segunda parte del problema
    	day_04/day_4_camp_cleanup.md: Se agrega el planteamiento de la segunda parte del problema

--- day_04/day4.py
+++ day_04/day4.py
@@ -14,3 +14,16 @@ with open('input', 'r') as f:
             n_subelements += 1
 
 print(f'Numero de subelementos {n_subelements}')
+
+#Part 1
+with open('input', 'r') as f:
+    n_subelements = 0
+    for l in f.readlines():
+        elf_1, elf_2 = l.strip().split(',')
+        range_1, range_2 = elf_1.split('-'), elf_2.split('-')
+        section_1 = set(range(int(range_1[0]), int(range_1[1])+1))
+        section_2 = set(range(int(range_2[0]), int(range_2[1])+1))
+        if section_1.intersection(section_2):
+            n_subelements += 1
+
+print(f'Numero de elemento solapados {n_subelements}')
